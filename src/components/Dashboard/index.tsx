a152a58d876532d08ab37fb105a37588b24b692a
Marco Carvalho de Pinho <marco.pinho@altimus.com.br>
2022-01-02 09:53:02 -0300

    Aulas

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Summary from "../Summary";
+
+import { Container } from "./styles";
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Summary>TExt</Summary>
+    </Container>
+  );
+};
+
+export default Dashboard;
