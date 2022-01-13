7c140dec880bc161e62da625a1fb1b4d373f8ab3
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-01-13 20:42:50 -0300

    Aula e correções

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,7 +1,5 @@
-import React from "react";
 import Summary from "../Summary";
 import TransactionsTable from "../TransactionsTable";
-
 import { Container } from "./styles";
 
 const Dashboard = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
+import { StrictMode } from "react";
+import { render } from "react-dom";
 import { App } from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById("root")
 );
