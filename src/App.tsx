2fcd01c786a82e7d63ac83d6eb3876b3c9474b79
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-01-18 20:38:34 -0300

    aula

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { useCallback, useState } from "react";
+import Modal from "react-modal";
 import Dashboard from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true);
+  }, []);
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <GlobalStyle />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
     </>
   );
 };
