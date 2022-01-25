6420ba0f0b95f005fce3928c5f4bd0a5f1d84eb6
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-01-25 19:10:19 -0300

    3 aulas

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionProps {
@@ -19,9 +20,21 @@ const NewTransactionModal = ({
   const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  const handleCreateNewTransaction = useCallback((event: FormEvent) => {
-    event.preventDefault();
-  }, []);
+  const handleCreateNewTransaction = useCallback(
+    (event: FormEvent) => {
+      event.preventDefault();
+
+      const data = {
+        title,
+        value,
+        category,
+        type,
+      };
+
+      api.post("/transactions", data);
+    },
+    [title, value, category, type]
+  );
 
   return (
     <Modal
